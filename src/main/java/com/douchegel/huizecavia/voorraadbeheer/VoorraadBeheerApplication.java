5571d279b2aeb78bdd307aeb716f444cd9444a96
beheer <huizecavia>
2023-10-23 16:32:46 +0200

    initieel

---
+++ src/main/java/com/douchegel/huizecavia/voorraadbeheer/VoorraadBeheerApplication.java
@@ -0,0 +1,13 @@
+package com.douchegel.huizecavia.voorraadbeheer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoorraadBeheerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoorraadBeheerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douchegel/huizecavia/voorraadbeheer/VoorraadBeheerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douchegel.huizecavia.voorraadbeheer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoorraadBeheerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
